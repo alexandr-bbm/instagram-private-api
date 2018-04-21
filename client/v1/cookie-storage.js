94d037c5ba57fa4c9b2b6736b6c15fc0e6271b95
Alexander Gazizov <alexandr.bbm@gmail.com>
2018-04-21 18:37:16 +0300

    Resolves CookieNotValidError 2

--- client/v1/cookie-storage.js
+++ client/v1/cookie-storage.js
@@ -5,13 +5,13 @@ var _ = require('lodash');
 
 
 function CookieStorage(cookieStorage) {
-    this.storage = cookieStorage;
+  this.storage = Promise.promisifyAll(cookieStorage);
 }
 
 
 Object.defineProperty(CookieStorage.prototype, "store", {
-    get: function() { return this.storage },
-    set: function(val) {}
+  get: function() { return this.storage },
+  set: function(val) {}
 });
 
 
@@ -19,76 +19,74 @@ Object.defineProperty(CookieStorage.prototype, "store", {
 var Exceptions = require('./exceptions');
 module.exports = CookieStorage;
 
-
-CookieStorage.prototype.getCookieValue = function (name) {
-    var self = this;
-    return new Promise(function(resolve, reject) {
-        self.storage.findCookie(CONSTANTS.COOKIE_DOMAIN, '/', name, function(err, cookie) {
-            if (err) return reject(err);
-            if (!_.isObject(cookie)) return reject(new Exceptions.CookieNotValidError(name));
-            resolve(cookie);
-        })
-    });
+CookieStorage.prototype.getCookieValue = function (name,host=CONSTANTS.COOKIEHOSTNAME) {
+  return this.storage.findCookieAsync(host, '/', name)
+    .then(cookie=>{
+    if(!_.isObject(cookie) && host === CONSTANTS.COOKIEHOSTNAME){
+    return this.getCookieValue(name,CONSTANTS.HOSTNAME)
+  }else if(!_.isObject(cookie)){
+    throw new Exceptions.CookieNotValidError(name);
+  }else{
+    return cookie;
+  }
+});
 };
 
-
 CookieStorage.prototype.putCookie = function (cookie) {
-    var args = _.toArray(arguments);
-    var self = this;
-    return new Promise(function (resolve, reject) {
-        self.storage.putCookie(cookie, resolve);    
+  var args = _.toArray(arguments);
+  var self = this;
+  return new Promise(function (resolve, reject) {
+    self.storage.putCookie(cookie, resolve);
+  })
+};
+
+CookieStorage.prototype.getCookies = function (host=CONSTANTS.COOKIEHOSTNAME) {
+  return this.storage.findCookiesAsync(host, '/')
+    .then(cookies=>{
+    if(!cookies && host === CONSTANTS.COOKIEHOSTNAME){
+    return this.getCookies(CONSTANTS.HOSTNAME)
+  }else {
+    return cookies || [];
+  }
+});
+};
+CookieStorage.prototype.getAccountId = function () {
+  var self = this;
+  return this.getCookieValue('ds_user_id')
+    .then(function(cookie) {
+      var id = parseInt(cookie.value);
+      if (_.isNumber(id) && !_.isNaN(id)) {
+        return id;
+      } else {
+        throw new Exceptions.CookieNotValidError("ds_user_id");
+      }
     })
 };
 
 
-CookieStorage.prototype.getCookies = function () {
-    var self = this;
-    return new Promise(function(resolve, reject) {
-        self.storage.findCookies(CONSTANTS.COOKIE_DOMAIN, '/', function(err, cookies){
-            if (err) return reject(err);
-            resolve(cookies || []);
-        })
-    });
-};
-
-
-CookieStorage.prototype.getAccountId = function () {
-    var self = this;
-    return this.getCookieValue('ds_user_id')
-        .then(function(cookie) {
-            var id = parseInt(cookie.value);
-            if (_.isNumber(id) && !_.isNaN(id)) {
-                return id;
-            } else {
-                throw new Exceptions.CookieNotValidError("ds_user_id");
-            }
-        })
-};
-
-
 CookieStorage.prototype.getSessionId = function () {
-    var currentTime = new Date().getTime();
-    return this.getCookieValue('sessionid')
-        .then(function(cookie) {
-            var acceptable = cookie.expires instanceof Date && cookie.expires.getTime() > currentTime;
-            if(acceptable) return cookie.value;
-            throw new Exceptions.CookieNotValidError("sessionid"); 
-        })
+  var currentTime = new Date().getTime();
+  return this.getCookieValue('sessionid')
+    .then(function(cookie) {
+      var acceptable = cookie.expires instanceof Date && cookie.expires.getTime() > currentTime;
+      if(acceptable) return cookie.value;
+      throw new Exceptions.CookieNotValidError("sessionid");
+    })
 };
 
 
 CookieStorage.prototype.removeCheckpointStep = function () {
-    var self = this;
-    return new Promise(function(resolve, reject) {
-        self.storage.removeCookie(CONSTANTS.COOKIE_DOMAIN, '/', 'checkpoint_step', function(err){
-            if (err) return reject(err);
-            resolve();
-        })
-    });
+  var self = this;
+  return new Promise(function(resolve, reject) {
+    self.storage.removeCookie(CONSTANTS.COOKIE_DOMAIN, '/', 'checkpoint_step', function(err){
+      if (err) return reject(err);
+      resolve();
+    })
+  });
 };
 
 
 
 CookieStorage.prototype.destroy = function () {
-    throw new Error("Mehtod destroy is not implemented")
+  throw new Error("Mehtod destroy is not implemented")
 }
